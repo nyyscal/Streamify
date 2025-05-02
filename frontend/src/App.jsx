18eb5e6c08d27415881cc068937aec836ead6979
Nischal <nyyscal541556@gmail.com>
2025-05-02 17:30:07 +0545

    Layout Components Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import CallPage from "./pages/CallPage.jsx"
 import {Toaster} from "react-hot-toast"
 import PageLoader from './components/PageLoader.jsx'
 import useAuthUser from './hooks/useAuthUser.js'
+import Layout from './components/Layout.jsx'
 
 function App() {
 
@@ -24,9 +25,13 @@ if(isLoading) return <PageLoader/>
 
   return (
     <> 
-      <div className='h-screen' >
+      <div className='h-screen' data-theme="forest">
         <Routes>
-          <Route path="/" element={isAuthenticated && isOnboarded ? (<HomePage/>):( <Navigate to={!isAuthenticated ?"/login":"/onboarding"}/>)}/>
+          <Route path="/" element={isAuthenticated && isOnboarded ? (
+            <Layout showSidebar={true}>
+              <HomePage/>
+            </Layout>
+            ):( <Navigate to={!isAuthenticated ?"/login":"/onboarding"}/>)}/>
           <Route path="/signup" element={!isAuthenticated ? <SignUpPage/>: <Navigate to={isOnboarded ? "/":"/onboarding"}/>}/>
           <Route path="/login" element={!isAuthenticated ? <LoginPage/>: <Navigate to={isOnboarded ? "/":"/onboarding"}/>}/>
           <Route path="/chat" element={isAuthenticated ?<ChatPage/>: <Navigate to="/login"/>}/>
